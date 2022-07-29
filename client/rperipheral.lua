8fd4965f412341ca4c2c5b668c1817658f1e97ea
Mason Gulu <mason.gulu@gmail.com>
2022-07-28 22:14:34 -0400

    Fixed an issue involving __call

--- client/rperipheral.lua
+++ client/rperipheral.lua
@@ -188,9 +188,8 @@ function rperipheral.hasType(name, peripehral_type)
   return peripehral_type == rperipheral.getType(name)
 end
 
-setmetatable(rperipheral, {__call=rperipheral.wrap})
-
 setmetatable(rperipheral, peripheralBackup)
+peripheralBackup.__call = rperipheral.wrap
 peripheralBackup.__index = peripheralBackup
 
 return rperipheral
